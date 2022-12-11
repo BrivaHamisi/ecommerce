bb26c5e4b3f99a7a5d1b42a6145c62a29283b2a1
Hamisi-Esq <brivahamisi@gmail.com>
2022-12-11 23:06:18 +0300

    Completed Add to Cart Functionality

--- store/views.py
+++ store/views.py
@@ -24,10 +24,13 @@ def cart(request):
           customer = request.user.customer
           order, created = Order.objects.get_or_create(customer=customer, complete=False)
           items = order.orderitem_set.all()
+          cartItems = order.get_cart_items
      else:
           items = []
-          order = {'get_cart_items':0, 'get_cart_total':0}
-     context = {'items':items, 'order':order}
+          order = {'get_cart_items':0, 'get_cart_total':0, 'shipping':False}
+          cartItems = order['get_cart_items']
+
+     context = {'items':items, 'order':order, 'cartItems':cartItems}
      return render(request, 'store/cart.html', context)
 
 def checkout(request):
@@ -35,10 +38,13 @@ def checkout(request):
           customer = request.user.customer
           order, created = Order.objects.get_or_create(customer=customer, complete=False)
           items = order.orderitem_set.all()
+          cartItems = order.get_cart_items
      else:
           items = []
-          order = {'get_cart_items':0, 'get_cart_total':0}
-     context = {'items':items, 'order':order}
+          order = {'get_cart_items':0, 'get_cart_total':0, 'shipping':False}
+          cartItems = order['get_cart_items']
+ 
+     context = {'items':items, 'order':order, 'cartItems':cartItems}
      return render(request, 'store/checkout.html', context)
 
 def updateItem(request):
